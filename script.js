0b7ce2cad5ea1b5f84b9664200f6dbcc0fb9cf76
Eugene Song <yougene42193@gmail.com>
2019-01-16 13:16:33 -0500

    formatting for URL's

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
-const GiantbombKey = ' ';
+const GiantbombKey = '4b9c2a43261b7540c1f412f7e3c3fec306c286a4';
 
-const YoutubeKey = ' ';
+const YoutubeKey = 'AIzaSyCPGJrolRVRl2XdQOfBQ2T__evHvY6iORA';
 
 const TwitchKey = ' ';
 
@@ -9,11 +9,17 @@ const GiantbombURL = 'https://www.giantbomb.com/api/games/';
 
 const YoutubeURL = 'https://www.googleapis.com/youtube/v3/videos';
 
-const TwitchURL = 'https://api.twitch.tv/kraken/streams/';
+const TwitchURL = 'https://api.twitch.tv/kraken/streams/game';
 
 function hideMainPage() {
   $('h1').css('display', 'none');
   $('form').css('top', '3.5%');
+}
+
+function formatURL(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params)}`);
+  return queryItems.join('&');
 }
 
 function displayResults() {
